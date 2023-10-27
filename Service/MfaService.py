c8b7faf5c9dde7261bbd44da49f074194686980d
saleems11 <iamme0ssa@gmail.com>
2023-10-27 16:05:22 +0300

    0002 fix mfa no able to validate in sync (#5)
    * Fix None Sync in MFA and login again errro
    
    MFA Sync, it could be that there is inSync bettwen the MFA server and the client so (+/- 1) mfa step are accepted.
    Also tryig to log in again Failed because the passwrod is already hashied, it was handled in the hashify function.
    
    * Add a comment and remove the debug line of code

--- Service/MfaService.py
+++ Service/MfaService.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pyotp
 import qrcode
 from PIL import Image
@@ -28,7 +30,7 @@ class MfaService:
         self.ShowQR(uri)
 
     def GenerateUserBaseUri(self, AccountName):
-        uri = self._TOTP.provisioning_uri(name=AccountName, issuer_name=f'{Consts.APP_NAME}-')
+        uri = self._TOTP.provisioning_uri(name=AccountName, issuer_name=f'{Consts.APP_NAME} ')
         return uri
 
     @staticmethod
@@ -42,7 +44,10 @@ class MfaService:
         im.show()
 
     def Validate(self, Pin):
-        return self._TOTP.verify(Pin)
+        # Pin is the mfa key
+        # datetime.now() - uses the local time of the machine, it must be the same timezone with the Mfa app.
+        # 1 - is used to handle cases where the diff bettwen the Mfa server/client is bigger than 30 sec.
+        return self._TOTP.verify(Pin, datetime.now(), 1)
 
     @staticmethod
     def getMfaKeyFromMfaKeyDecrebtedKey(mfaKeyDecrebtedKey):

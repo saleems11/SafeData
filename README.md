55da8ef118612ddf85aa505d630e7824b810c765
saleems11 <iamme0ssa@gmail.com>
2023-12-08 17:13:39 +0200

    0006 change encreption algorithem (#11)
    * update readme on why AES is used
    
    * add more justification regarding AES
    
    * add more to do
    
    * more comments to encryption service

--- README.md
+++ README.md
@@ -2,13 +2,20 @@
 Using the registraion password, the MFA token is encrepted
 
 #### Some Information regarding how the SafeData application Stores the password
-Using the user password a hash key is generated, the hashed password is used to encrypt the MFA key,
+Using the user password a hash key is generated in the size of 256 bits, the hashed 
+password is used to encrypt the MFA key via AES-256-CBC,
 then this encrypted key is stored in a file.
 
 ##### Authentication Process
 The authentication is required for (Encryption/Decryption)!
 The User Enter his password and Mfa pin, the password is Hashified
 then the Mfa key is decrypted, using the MFA key the MFA pin is validated. 
+
+## Encryption Used:
+**AES**, based on:  
+https://www.arcserve.com/blog/5-common-encryption-algorithms-and-unbreakables-future  
+**CBC**, based on:
+https://security.stackexchange.com/questions/184305/why-would-i-ever-use-aes-256-cbc-if-aes-256-gcm-is-more-secure
 
 ###### Encryption Process
 Using the hashed mfa key the passwords are encrypted, also Gebbrish data is added.
@@ -32,5 +39,10 @@ encrypted value.
 **1. API Support**  
 **2. More Secure Password Saving Method**
 
+
+## To Do:
+1. Allow multi Users - Done
+2. Save More Data
+3. Save metaData Per User
 
 Saleem Saiegh
--- Service/EncryptionService.py
+++ Service/EncryptionService.py
@@ -6,7 +6,8 @@ from Service.GibberishService import GibberishService
 
 
 class EncryptionService:
-
+    # AES modes:
+    # https://onboardbase.com/blog/aes-encryption-decryption/
     def __init__(self):
         pass
 
@@ -23,6 +24,21 @@ class EncryptionService:
         return iv + cipher.encrypt(gibberishDataInBytes)
 
     @staticmethod
+    def encyptV2(message:str, key):
+        # encryption based on the Key size
+        # add Gibberish Part
+        # will not be used, but it is here in case needed
+        # code based on(not by the best practices, it was intended to be used for testing):
+        # https://asecuritysite.com/encryption/aes_gcm#:~:text=AES%20GCM%20(Galois%20Counter%20Mode,it%20does%20not%20require%20padding.
+        gibberishData = GibberishService.addGibberishToData(message)
+        gibberishDataInBytes = bytes(gibberishData, Consts.encoding)
+
+        # add IV
+        encobj = AES.new(key, AES.MODE_GCM)
+        ciphertext, authTag = encobj.encrypt_and_digest(gibberishDataInBytes)
+        return (ciphertext, authTag, encobj.nonce)
+
+    @staticmethod
     def decrypt(ciphertext, key):
         # Decryption Part
         iv = ciphertext[:AES.block_size]
@@ -31,6 +47,19 @@ class EncryptionService:
 
         # Clean up part
         textWithoutBytes = plaintext.rstrip(b"\0")
+        decodedTextStr = textWithoutBytes.decode(Consts.encoding)
+        cleanplaintextDec = GibberishService.removeGibberishFromData(decodedTextStr)
+
+        return cleanplaintextDec
+
+    @staticmethod
+    def decryptV2(ciphertext, key):
+        (ciphertext, authTag, nonce) = ciphertext
+        encobj = AES.new(key, AES.MODE_GCM, nonce)
+        decryptedData = (encobj.decrypt_and_verify(ciphertext, authTag))
+
+        # Clean up part
+        textWithoutBytes = decryptedData.rstrip(b"\0")
         decodedTextStr = textWithoutBytes.decode(Consts.encoding)
         cleanplaintextDec = GibberishService.removeGibberishFromData(decodedTextStr)
 
